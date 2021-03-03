5d9732690bc6f8846d4218b09f1c68acf16223d0
Mohan Devarajan <76619665+MohanDevarajan@users.noreply.github.com>
2021-03-03 21:58:07 +0000

    Create Scirpt.sh

---
+++ Scirpt.sh
@@ -0,0 +1,3 @@
+gcloud spanner instances create mohan-spanner-id  --config=regional-europe-west2  --description=mohan-spanner-id --nodes=1
+gcloud spanner databases create mohan_database --instance=mohan-spanner-id --ddl='CREATE TABLE ImportTest (importid INT64 NOT NULL, strCol STRING(100) NOT NULL, byteCol BYTES(100) NO
+T NULL, dateCol DATE NOT NULL, tsCol TIMESTAMP NOT NULL, intCol INT64 NOT NULL, floatCol FLOAT64 NOT NULL,) PRIMARY KEY (importid)'
