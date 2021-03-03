2b160f69c844cdb6812d09d18955328706205f29
Mohan Devarajan <76619665+MohanDevarajan@users.noreply.github.com>
2021-03-03 21:57:37 +0000

    Delete Script.sh

--- Script.sh
+++
@@ -1,2 +0,0 @@
-gcloud spanner databases create mohan_database --instance=mohan-spanner-id --ddl='CREATE TABLE ImportTest (importid INT64 NOT NULL, strCol STRING(100) NOT NULL, byteCol BYTES(100) NO
-T NULL, dateCol DATE NOT NULL, tsCol TIMESTAMP NOT NULL, intCol INT64 NOT NULL, floatCol FLOAT64 NOT NULL,) PRIMARY KEY (importid)
