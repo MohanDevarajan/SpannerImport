3fafcc2c1cce31ce244a758e75c2920adebe101e
Mohan Devarajan <76619665+MohanDevarajan@users.noreply.github.com>
2021-03-05 18:29:33 +0000

    Update command_stg_facility_x_party.cmd

--- command_stg_facility_x_party.cmd
+++ command_stg_facility_x_party.cmd
@@ -1,1 +1,1 @@
-python new.py --instance_id=rvsrdl-demo-spanner-id --database_id=rvsrdl_demo_database --bucket_name=rvsrdldemo --table_id=STG_FACILITY_X_PARTY --batchsize=1600 --data_file=STG_FACILITY_X_PARTY.csv --format_file=stg_facility_x_party.fmt
+python3 new.py --instance_id=rvsrdl-demo-spanner-id --database_id=rvsrdl_demo_database --bucket_name=rvsrdldemo --table_id=STG_FACILITY_X_PARTY --batchsize=1600 --data_file=STG_FACILITY_X_PARTY.csv --format_file=stg_facility_x_party.fmt
